cd4e17f777a92be02b910be6b17c2fa56c161f8c
Martin Sandiford <ms@mcdev.com.au>
2026-01-31 21:06:00 +0000

    Implement all YNAQ responses

--- test_V_verify.py
+++ test_V_verify.py
@@ -2,5 +2,18 @@ from systest import simple_pexpect_test
 
 #  LEADING PARAMETER: [none,   ,   ,    ,    ,   ,   ,   ] V
 
-def test_no_leading_parameter():
-    simple_pexpect_test("\\exludwig\ry\\q", "v&Yes?&[i/yes/:i/no/]", "yesv&Yes?&[i/yes/:i/no/]\n")
+def test_no_leading_parameter_yes():
+    cmd = "v&Yes?&[i/yes/:i/no/]"
+    simple_pexpect_test("\\exludwig\ry\\q", cmd, f"yes{cmd}\n")
+
+def test_no_leading_parameter_no():
+    cmd = "v&Yes?&[i/yes/:i/no/]"
+    simple_pexpect_test("\\exludwig\rn\\q", cmd, f"no{cmd}\n")
+
+def test_no_leading_parameter_always():
+    cmd = "3(v&Yes?&[i/yes/:i/no/])"
+    simple_pexpect_test("\\exludwig\ra\\q", cmd, f"yesyesyes{cmd}\n")
+
+def test_no_leading_parameter_quit():
+    cmd = "3(v&Yes?&[i/yes/:i/no/])"
+    simple_pexpect_test("\\exludwig\rqx\\q", cmd, f"x{cmd}\n")
