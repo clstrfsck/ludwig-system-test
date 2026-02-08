2b549124065c6b289bda80d117e67968c04f0034
Martin Sandiford <ms@mcdev.com.au>
2026-02-08 22:57:11 +0000

    Additional multi-line span test

--- test_SC_span_copy.py
+++ test_SC_span_copy.py
@@ -13,3 +13,6 @@ def test_pint_leading_parameter():
 
 def test_no_leading_parameter_deref():
     simple_edit_test("sa/t1/t2/sa/t2/abc/sc/$t1$/", "def\n", "abcdef\n")
+
+def test_no_leading_parameter_deref_2():
+    simple_edit_test("2a>jm<a@sd/t/i/$t$/", "abc\ndef\nghi\n", "abc\ndef\nghiabc\ndef\nghi\n")
