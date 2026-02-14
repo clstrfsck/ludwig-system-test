19ea607fe2d820bf3f0fc3f74f837f5e5b163470
Martin Sandiford <ms@mcdev.com.au>
2026-02-14 22:22:50 +0000

    Remove H test

--- test_H_help.py
+++
@@ -1,6 +0,0 @@
-from systest import simple_pexpect_test
-
-#  LEADING PARAMETER: [none,   ,   ,    ,    ,   ,   ,   ] H
-
-def test_no_leading_parameter_yes():
-    simple_pexpect_test("\\h\r\r\\d\\q", "abc\n", "bc\n", "Main Help Menu")
